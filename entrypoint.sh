147c8c318d315d6434259b51c2703ebbed017f95
Rafael R <rrmmbmg903@gmail.com>
2023-05-05 18:36:08 -0300

    fix: Fix several issues in SSH connection script
    This commit addresses several issues with the SSH connection script, including:
    
    - Fixing an error related to an out of range value
    - Correcting a missing variable in the log message
    - Removing the use of the goto command, which is not available on all systems
    - Simplifying the logic for the MODE variable
    - Removing duplicate code for copying the SSH key to a non-root directory
    - Moving the assignment of the SSH_PID variable to after the ssh command, to capture the correct process ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
 set -e
 
+# Copy the ssh key to a non-root directory
 if [[ ! -e "/ssh/id_rsa" ]];then
  if [ ! -f "$SSH_KEY" ]; then
   echo "SSH key file not found at: $SSH_KEY"
   exit 1
  fi
- # Copy the ssh key to a non-root directory
  mkdir -p /ssh
  cp "$SSH_KEY" /ssh/id_rsa
  chmod 400 /ssh/id_rsa
 fi
 
-if [ $MODE -ne "REMOTE" ] && [ $MODE -ne "LOCAL" ]; then
-  echo "Environment variable MODE is not corretly set. Possible values: 'REMOTE', 'LOCAL'."
+if [ "$MODE" != "REMOTE" ] && [ "$MODE" != "LOCAL" ]; then
+  echo "Environment variable MODE is not correctly set. Possible values: 'REMOTE', 'LOCAL'."
   exit 1
 fi
 
@@ -22,30 +21,22 @@ echo "Attempting to connect to $SSH_HOST at $(date)"
 
 while true; do
   # Run the SSH command with ExitOnForwardFailure option
- if [ $MODE -eq "REMOTE" ];then
+ if [ "$MODE" = "REMOTE" ];then
   ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL 0.0.0.0:"$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT" "$SSH_USER@$SSH_HOST" &
-  SSH_PID=$!
- elif [ $MODE -eq "LOCAL" ];then
+ elif [ "$MODE" = "LOCAL" ];then
   ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL "$LOCAL_HOST:$LOCAL_PORT:$REMOTE_PORT" "$SSH_USER@$SSH_HOST" &
-  SSH_PID=$!
  fi
+ SSH_PID=$!
 
   # Wait for the SSH command to exit
   wait $SSH_PID
   # Add logs for connection status
   if [ $? -eq 0 ]; then
-    while true; do
-    goto logging_ok
-    sleep 60; done
+    echo "SSH connection to $SSH_HOST is active at $(date)"
   else
-    goto logging_error
+    echo "SSH connection to $SSH_HOST is not active at $(date)"
   fi
 
   # Wait for a while before attempting to reconnect
   sleep 5
 done
-
-logging_error:
-    echo "SSH connection to $SSH_HOST is not active at $(date)"
-logging_ok:
-    echo "SSH connection to $SSH_HOST is active at $(date)"
