cc762ee22b809d5c434123982ad7f6b43a3b6f0d
Rafael R <rrmmbmg903@gmail.com>
2023-05-08 18:35:24 -0300

    fix: Fix several issues in entrypoint This commit addresses several issues with entrypoint, including: - This script is executed in /bin/sh - The mode with what private key is a included in non-root directory - Now  private key is obtained in environment variable SSH_KEY
    This script is now simpler

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
-set -e
+#!/bin/sh
 
+echo $(id) | xargs | awk '{print $1}'
 # Copy the ssh key to a non-root directory
-if [[ ! -e "/ssh/id_rsa" ]];then
- if [ ! -f "$SSH_KEY" ]; then
-  echo "SSH key file not found at: $SSH_KEY"
-  exit 1
- fi
- mkdir -p /ssh
- cp "$SSH_KEY" /ssh/id_rsa
- chmod 400 /ssh/id_rsa
-fi
-
+mkdir -p /ssh
+echo -e "$SSH_KEY" > /ssh/id_rsa
+chmod 400 /ssh/id_rsa
 if [ "$MODE" != "REMOTE" ] && [ "$MODE" != "LOCAL" ]; then
   echo "Environment variable MODE is not correctly set. Possible values: 'REMOTE', 'LOCAL'."
   exit 1
@@ -22,9 +16,17 @@ echo "Attempting to connect to $SSH_HOST at $(date)"
 while true; do
   # Run the SSH command with ExitOnForwardFailure option
  if [ "$MODE" = "REMOTE" ];then
-  ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL 0.0.0.0:"$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT" "$SSH_USER@$SSH_HOST" &
+  echo "Running: ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NR $REMOTE_HOST:$REMOTE_PORT:$LOCAL_HOST:$LOCAL_PORT $SSH_USER@$SSH_HOST &"
+  ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NR $REMOTE_HOST:$REMOTE_PORT:$LOCAL_HOST:$LOCAL_PORT $SSH_USER@$SSH_HOST &
+  # ssh -NR 0.0.0.0:1111:localhost:3306
+  #   {LISTEN HOST}:{LISTEN PORT}:{BINDING HOST}:{BINDING PORT}
+  # This example listening port 1111 remotely, use tunnel SSH to binding a local port
  elif [ "$MODE" = "LOCAL" ];then
-  ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL "$LOCAL_HOST:$LOCAL_PORT:$REMOTE_PORT" "$SSH_USER@$SSH_HOST" &
+  echo "Running: ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL $LOCAL_HOST:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST &"
+  ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -i /ssh/id_rsa -NL $LOCAL_HOST:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST &
+  # ssh -NL 0.0.0.0:1111:localhost:3306
+  # {LISTEN HOST}:{LISTEN PORT}:{BINDING HOST}:{BINDING PORT}
+  # This example listening port 1111 localy, use tunnel SSH to binding a remote port
  fi
  SSH_PID=$!
 
